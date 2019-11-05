90f28707c6d8ff8436666d7d78631246f2a2be23
Das PeTe <daspetemail@gmail.com>
2019-11-05 23:33:42 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="page">
+        PAGE
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.page {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+}
+
+</style>
